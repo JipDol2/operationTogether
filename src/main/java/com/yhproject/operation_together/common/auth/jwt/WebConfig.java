8b936b10a480148f455a3dad553c8d7f8a772d8b
seongju lee <gch03915@naver.com>
2022-03-28 00:17:47 +0900

    modify Interceptor+WebConfig

---
+++ src/main/java/com/yhproject/operation_together/common/auth/jwt/WebConfig.java
@@ -0,0 +1,22 @@
+package com.yhproject.operation_together.common.auth.jwt;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtTokenInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/","/login","/api/**","/css/**","/js/**");
+    }
+
+    @Bean
+    public JwtTokenInterceptor jwtTokenInterceptor(){
+        return new JwtTokenInterceptor();
+    }
+}
